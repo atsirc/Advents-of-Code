a361163abe016621b78012b06938f71598fb4fe2
atsirc <christa.hannuksela@gmail.com>
2021-12-04 19:11:26 +0200

    removed unnescessary files

--- AOC_2019/test.js
+++
@@ -1,3 +0,0 @@
-const fs = require("fs")
-const txt = fs.readFileSync("dag_7.txt", {encoding: 'utf-8'})
-console.log("txt")
--- aoc2018/part_22/kråkkod.py
+++
@@ -1,9 +0,0 @@
-def fs(n):
-    a, b = 0, 1
-    f=[0]
-    for i in range(n):
-        a, b = b, a + b
-        f.append(a)
-    return f
-f=fs(1000)
-print(f)
